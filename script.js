2c6b6385dc632335e608ed849dd65891cfaefe3e
SahilShaikhh <123715018+SahilShaikhh@users.noreply.github.com>
2024-12-09 14:33:59 +0530

    Update script.js
    js not working

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 document.addEventListener('DOMContentLoaded', function() {
     const contactForm = document.getElementById('contact-form');
     contactForm.addEventListener('submit', function(event) {
+        console.log('vabhad');
         event.preventDefault();
         alert('Message sent!');
     });
