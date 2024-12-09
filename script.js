7e6befc4b55541c12dc820e105d0d741da1c71a4
SahilShaikhh <123715018+SahilShaikhh@users.noreply.github.com>
2024-12-09 14:37:08 +0530

    Update script.js
    removal of vabhads

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
     const contactForm = document.getElementById('contact-form');
     contactForm.addEventListener('submit', function(event) {
-        console.log('vabhad');
         event.preventDefault();
         alert('Message sent!');
     });
